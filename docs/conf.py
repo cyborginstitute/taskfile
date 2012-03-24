8ae903440ba062775199905db8bde7d4d5ef7f62
tycho garen <garen@tychoish.com>
2012-03-24 10:56:39 -0400

    renaming docs files to .txt and cleaning up conf

--- docs/conf.py
+++ docs/conf.py
@@ -10,28 +10,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".ext"))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ["sphinx.ext.todo", "make_domain", "emacs_domain"]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
 
-# The suffix of source filenames.
-source_suffix = '.rst'
-
-# The master toctree document.
+source_suffix = '.txt'
 master_doc = 'index'
 
-# General information about the project.
 project = u'Taskfile: A Tasklist Compiler'
 copyright = u'2012, Sam Kleinman'
 
 version = '0.1'
 release = ''
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
 exclude_patterns = []
 
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 add_function_parentheses = False
 
@@ -40,17 +31,10 @@ add_function_parentheses = False
 html_theme = 'cyborg'
 html_theme_path = ['themes']
 
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-#html_title = None
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
+html_title = 'Taskfile, a Tasklist Compiler'
+html_short_title = 'Taskfile'
+
 #html_logo = None
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
 #html_favicon = None
 
 html_static_path = ['source/.static']
@@ -59,44 +43,32 @@ html_use_smartypants = True
 # If false, no module index is generated.
 #html_domain_indices = True
 # If false, no index is generated.
-#html_use_index = True
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-# If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_use_index = True
+html_split_index = False
+html_show_sourcelink = True
+html_show_sphinx = False
+html_show_copyright = True
 
-# Output file base name for HTML help builder.
 htmlhelp_basename = 'cyborg-institute'
 
 # -- Options for LaTeX output --------------------------------------------------
 
-# The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'letter'
+latex_font_size = '10pt'
 
-# The font size ('10pt', '11pt' or '12pt').
-#latex_font_size = '10pt'
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
+# (source start file, target name, title, author, documentclass [howto/manual]).
   ('index', 'taskfile.tex', u'Taskfile: A Tasklist Compiler',
    u'Sam Kleinman', 'howto'),
 ]
 
-# The name of an image file (relative to this directory) to place at the top of the title page.
 #latex_logo = None
 
 # -- Options for manual page output --------------------------------------------
 
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
 man_pages = [
-  ('index', 'Taskfile', u'Taskfile: A Tasklist Compiler',
-   [u'Sam Kleinman'], 1)
+# (source start file, name, description, authors, manual section).
+  ('index', 'Taskfile', u'Taskfile: A Tasklist Compiler', [u'Sam Kleinman'], 1)
 ]
 
 # -- Options for Epub output ---------------------------------------------------
@@ -113,4 +85,4 @@ epub_copyright = u'2011, Sam Kleinman'
 #epub_tocdup = True
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+# intersphinx_mapping = {'http://docs.python.org/': None}
